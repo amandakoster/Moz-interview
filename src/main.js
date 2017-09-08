42a151af4f368d31247e922fbd0cf0fa0b09aab9
Amanda Koster <amandakoster@gmail.com>
2017-09-07 19:51:17 -0700

    on to the form

--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom'
 
+
+class WagesForm extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      jobTitle: '',
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
 class App extends Component {
   render() {
     return (
       <div>
       <h1>Hello World</h1>
+      <h2>testing</h2>
       </div>
 
     );
