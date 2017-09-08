6c5a4f5eee1d974e8050d871e3f5937070eba35d
Amanda Koster <amandakoster@gmail.com>
2017-09-07 19:26:41 -0700

    hello world is up

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Application entrypoint.
+
+// Load up the application styles
+// require("../styles/application.scss");
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from 'main.js';
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
