00734af408d1d71e2a2a4bbb000ca8dffb974d34
Amanda Koster <amandakoster@gmail.com>
2017-09-08 14:33:22 -0700

    working on geting data to DOM

--- src/main.js
+++ src/main.js
@@ -3,53 +3,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import superagent from 'superagent';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      jobtitle: [],
-      femaleRate: 0,
-      maaleRate: 0,
-      results: null,
-      jobtitleSelected: null,
-      jobtitleError: null,
-    };
-    this.search = this.search.bind(this);
-  }
+const API_URL = 'https://data.seattle.gov/api/views/cf52-s8er/rows.json?api_key=SCC1c0Cove7ypmBeuf3dTX2WZOk6qEfCAki6MoNi';
+let searchSeattle;
 
-  search(jobtitle, limit) {
-    superagent.get(`https://data.seattle.gov/api/views/cf52-s8er/rows.json?api_key=SCC1c0Cove7ypmBeuf3dTX2WZOk6qEfCAki6MoNi`)
-      .then(res => {
-
-        console.log(res);
-        this.setState({
-          results: res.body,
-          serchErrorMessage: null,
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        this.setState({
-          jobtitle: res.body.data.children,
-        });
-        console.log(this.state.jobtitle[0].data.title);
-      });
-  }
-
-  render() {
-    return (
-      <main>
-        <div>
-          <h1></h1>
-          <SearchForm search = {this.search} />
-          <div className="inputBox">
-            <SearchResultList jobtitle = {this.state.jobtitle} />
-          </div>
-        </div>
-      </main>
-    );
-  }
-}
 
 //let renderIf = (test, component) => test? component ; undefined
 
@@ -57,26 +13,28 @@ class SearchForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchText: '',
+      textInput: '',
+      jobtitle: '',
+
     };
     this.handelSubmit = this.handelSubmit.bind(this);
     this.handelChange = this.handelChange.bind(this);
   }
 
   handelChange(e) {
-    this.setState({searchText: e.target.value});
+    this.setState({textInput: e.target.value});
   }
 
   handelSubmit(e) {
     e.preventDefault();
     console.log('handlesubmit');
-    this.props.search(this.state.searchText);
+    this.props.searchSeattle(this.state.textInput);
   }
   render() {
     return (
       <form onSubmit = {this.handelSubmit}>
         <input type = 'text'
-          value = {this.state.searchText}
+          value = {this.state.textInput}
           onChange = {this.handelChange}/>
         <button type='submit'> search button </button>
       </form>
@@ -89,6 +47,7 @@ class SearchResultList extends React.Component {
     super(props);
   }
   render() {
+    console.log('HIT SearchResultList');
     return (
       <div>
         <h1> List</h1>
@@ -104,6 +63,57 @@ class SearchResultList extends React.Component {
         }
         </ul>
       </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobtitle: [],
+      femaleRate: 0,
+      maaleRate: 0,
+      results: null,
+      jobtitleSelected: null,
+      jobtitleError: null,
+    };
+    this.searchSeattle = this.searchSeattle.bind(this);
+  }
+
+  searchSeattle(jobtitle, limit) {
+    superagent.get(`${API_URL}${jobtitle}.json`)
+      .then(res => {
+        console.log(res, 'RES');
+        this.setState({
+          results: res.body,
+          serchErrorMessage: null,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          jobtitle: res.body.data.children,
+        });
+        console.log(this.state.jobtitle[3].data.title);
+      });
+  }
+
+  render() {
+    console.log('HIT App');
+    return (
+      <main>
+        <div>
+          <h1>Hit APP</h1>
+          <SearchForm
+            searchSeattle={this.searchSeattle}
+            hasError={this.state.hasError}/>
+
+          <div className="inputBox">
+            <SearchResultList jobtitle = {this.state.jobtitle} />
+          </div>
+        </div>
+      </main>
     );
   }
 }
