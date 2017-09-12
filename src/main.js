7f53f73e7ba82c2eb5459b89fe3743bf388bceba
Amanda Koster <amandakoster@gmail.com>
2017-09-12 16:38:59 -0700

    cleaning up code

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import ReactDom from 'react-dom';
 import superagent from 'superagent';
 import Data from './components/data-sort/index.js';
 import './style/_main.scss';
-
-
 const API_URL = 'https://data.seattle.gov/api/views/cf52-s8er/rows.json?api_key=SCC1c0Cove7ypmBeuf3dTX2WZOk6qEfCAki6MoNi';
 
 class App extends React.Component {
@@ -20,7 +18,6 @@ class App extends React.Component {
       </div>
     );
   }
-
 }
 
 const container = document.getElementById('root');
