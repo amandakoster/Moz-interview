69e25796584fe2c7c24c6f7881396869e8717a9d
Amanda Koster <amandakoster@gmail.com>
2017-09-12 19:34:38 -0700

    saftey commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import superagent from 'superagent';
-<<<<<<< HEAD
 import Data from './components/data';
 
-
-
-=======
-import Data from './components/data-sort/index.js';
-import './style/_main.scss';
->>>>>>> df55db6963c1daa76265d457f6b25b9829f61f6b
 const API_URL = 'https://data.seattle.gov/api/views/cf52-s8er/rows.json?api_key=SCC1c0Cove7ypmBeuf3dTX2WZOk6qEfCAki6MoNi';
 
 class App extends React.Component {
