7654a1d3170f517dbd2b821306a5d129eedf20a2
Amanda Koster <amandakoster@gmail.com>
2017-09-14 18:25:03 -0700

    figured out how to deploy a react app with firebase

--- public/main.js
+++ public/main.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import superagent from 'superagent';
 import Data from './data.js';
-// const functions = require('firebase-functions');
+import * as firebase from 'firebase';
+
+// <script src="https://www.gstatic.com/firebasejs/4.3.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAoywsbQUHk1Ld6g6cYOJ9K612bbeqCLJk",
+    authDomain: "seattle-wage-by-gender.firebaseapp.com",
+    databaseURL: "https://seattle-wage-by-gender.firebaseio.com",
+    projectId: "seattle-wage-by-gender",
+    storageBucket: "",
+    messagingSenderId: "535226148346"
+  };
+  firebase.initializeApp(config);
+</script>
+
 
 const API_URL = 'https://data.seattle.gov/api/views/cf52-s8er/rows.json?api_key=SCC1c0Cove7ypmBeuf3dTX2WZOk6qEfCAki6MoNi';
 
